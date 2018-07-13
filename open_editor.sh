7f0bacc757ac99d6708b55a8c8700cc1c7ee14f6
Ethan Arns <narkro555@gmail.com>
2018-07-12 21:09:41 -0700

    Initial file upload

---
+++ open_editor.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cloudshell edit-files ~/src/ethan-web-project-v1/java_gae_quickstart-2018-07-12-14-34/src/main/java/myapp/MainServlet.java
---
+++ recompile_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ~/src/ethan-web-project-v1/java_gae_quickstart-2018-07-12-14-34/
+mvn clean appengine:deploy
