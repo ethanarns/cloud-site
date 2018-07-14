8601c4de2b95c9db1714fbec066eb41cbe83c715
Ethan Arns <narkro555@gmail.com>
2018-07-13 20:05:37 -0700

    Giant mirror update

--- src/ethan-web-project-v1/java_gae_quickstart-2018-07-12-14-34/src/main/java/myapp/MainServlet.java
+++ src/ethan-web-project-v1/java_gae_quickstart-2018-07-12-14-34/src/main/java/myapp/MainServlet.java
@@ -17,16 +17,44 @@
 package myapp;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
-    resp.setContentType("text/plain");
-    // { "name" : "Ethan Arns"}
-    resp.getWriter().println("{ \"name\": \"Ethan Arns\" }");
-  }
+    
+        String ip = "" + req.getRemoteAddr();
+        
+        // Browser detecter
+        String header_user = req.getHeader("User-Agent").toLowerCase();
+        if(header_user.indexOf("firefox") != -1)
+            header_user = "Firefox";
+        else if (header_user.indexOf("chrome") != -1)
+            header_user = "Chrome";
+        else if (header_user.indexOf("opera") != -1)
+            header_user = "Opera";
+        else if (header_user.indexOf("safari") != -1)
+            header_user = "Safari";
+        else if (header_user.indexOf("ie") != -1)
+            header_user = "Internet Explorer";
+        else
+            header_user = "Unknown";
+        
+        // Build JSON output
+        HashMap<String, String> dict = new HashMap<String, String>();
+        dict.put("name","Ethan Arns");
+        dict.put("ip", ip);
+        dict.put("browser", header_user);
+        
+        // Send JSON response
+        resp.setContentType("text/plain");
+        resp.getWriter().println(new JSONObject(dict));
+    }
 }
